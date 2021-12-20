fae2160d08c878d0043c1e8ce7399eb7451ac5e8
mohamedmmc <46996751+mohamedmmc@users.noreply.github.com>
2021-12-20 15:15:58 +0100

    Cloudinary

--- routes/user.js
+++ routes/user.js
@@ -298,7 +298,7 @@ router.get('/resendConfirmation/:id',getUserById,async (req,res,next) =>{
             }
             
         });
-        res.json({message:"ok"})
+        res.json({message:tokne.token})
     } catch (error) {
         console.log(error);
     }
