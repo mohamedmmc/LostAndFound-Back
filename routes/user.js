0e57f258fbb6823c88d2f5ae767f8f0426010e11
mohamedmmc <46996751+mohamedmmc@users.noreply.github.com>
2021-12-29 03:32:10 +0100

    html response added

--- routes/user.js
+++ routes/user.js
@@ -620,8 +620,8 @@ router.post ('/Social',multer,async (req,res) => {
                     <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
                         <tr>
                             <td bgcolor="#ffffff" align="left" style="padding: 20px 30px 40px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
-                                <p style="margin: 0;">We're excited to have you get started. First, you need to confirm your account. Just press the button below.</p>
-                            </td>
+                                <p style="margin: 0;">Merci d'avoir creer un compte dans LostAndFound. vous pouvez confirmer votre compte en appuyant sur le bouton suivant.</p>
+                                </td>
                         </tr>
                         <tr>
                             <td bgcolor="#ffffff" align="left">
@@ -640,48 +640,14 @@ router.post ('/Social',multer,async (req,res) => {
                         </tr> <!-- COPY -->
                         <tr>
                             <td bgcolor="#ffffff" align="left" style="padding: 0px 30px 0px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
-                                <p style="margin: 0;">If that doesn't work, copy and paste the following link in your browser:</p>
+                                <p style="margin: 0;">Vous pourrez par la suite ajouter vos articles LostAndFound ainsi que discuter avec d'autres utilisateurs.</p>
                             </td>
                         </tr> <!-- COPY -->
-                        <tr>
-                            
-                        </tr>
-                        <tr>
-                            <td bgcolor="#ffffff" align="left" style="padding: 0px 30px 20px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
-                                <p style="margin: 0;">If you have any questions, just reply to this email—we're always happy to help out.</p>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td bgcolor="#ffffff" align="left" style="padding: 0px 30px 40px 30px; border-radius: 0px 0px 4px 4px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
-                                <p style="margin: 0;">Cheers,<br>BBB Team</p>
-                            </td>
-                        </tr>
+                        
                     </table>
                 </td>
             </tr>
-            <tr>
-                <td bgcolor="#f4f4f4" align="center" style="padding: 30px 10px 0px 10px;">
-                    <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
-                        <tr>
-                            <td bgcolor="#FFECD1" align="center" style="padding: 30px 30px 30px 30px; border-radius: 4px 4px 4px 4px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
-                                <h2 style="font-size: 20px; font-weight: 400; color: #111111; margin: 0;">Need more help?</h2>
-                                <p style="margin: 0;"><a href="#" target="_blank" style="color: #FFA73B;">We&rsquo;re here to help you out</a></p>
-                            </td>
-                        </tr>
-                    </table>
-                </td>
-            </tr>
-            <tr>
-                <td bgcolor="#f4f4f4" align="center" style="padding: 0px 10px 0px 10px;">
-                    <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
-                        <tr>
-                            <td bgcolor="#f4f4f4" align="left" style="padding: 0px 30px 30px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 400; line-height: 18px;"> <br>
-                                <p style="margin: 0;">If these emails get annoying, please feel free to <a href="#" target="_blank" style="color: #111111; font-weight: 700;">unsubscribe</a>.</p>
-                            </td>
-                        </tr>
-                    </table>
-                </td>
-            </tr>
+           </tr>
         </table>
     </body>
     
@@ -1263,24 +1229,10 @@ router.get('/confirmation/:email/:token', async (req, res, next) => {
                                             <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
                                                 <tr>
                                                     <td bgcolor="#ffffff" align="left" style="padding: 20px 30px 40px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
-                                                        <p style="margin: 0;">Pour verifier votre compte merci de cliquer sur le bouton suivant</p>
+                                                        <p style="margin: 0;">Votre compte est desormais active</p>
                                                     </td>
                                                 </tr>
-                                                <tr>
-                                                    <td bgcolor="#ffffff" align="left">
-                                                        <table width="100%" border="0" cellspacing="0" cellpadding="0">
-                                                            <tr>
-                                                                <td bgcolor="#ffffff" align="center" style="padding: 20px 30px 60px 30px;">
-                                                                    <table border="0" cellspacing="0" cellpadding="0">
-                                                                        <tr>
-                                                                            <td align="center" style="border-radius: 3px;" bgcolor="#FFA73B"><a href="#" target="_blank" style="font-size: 20px; font-family: Helvetica, Arial, sans-serif; color: #ffffff; text-decoration: none; color: #ffffff; text-decoration: none; padding: 15px 25px; border-radius: 2px; border: 1px solid #FFA73B; display: inline-block;">Confirmer le compte</a></td>
-                                                                        </tr>
-                                                                    </table>
-                                                                </td>
-                                                            </tr>
-                                                        </table>
-                                                    </td>
-                                    </tr>
+                                               
                                 </table>
                             </body>
                             
