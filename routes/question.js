a7457a7d81caf92e33f8b88901aebbf85563c8e5
mohamedmmc <46996751+mohamedmmc@users.noreply.github.com>
2021-12-31 14:06:34 +0100

    question reponse

--- routes/question.js
+++ routes/question.js
@@ -6,7 +6,7 @@ const Article = require('../models/article')
 //getting all
 router.get ('/', async (req,res) => {
     try {
-        const question = await Question.find().populate('article').populate('reponse')
+        const question = await Question.find()
         res.json(question)
     } catch (error) {
         res.status(500).json({message: error.message})
