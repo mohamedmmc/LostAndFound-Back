3ce3456f8848eccc7df16272a8323082051ed800
mohamedmmc <46996751+mohamedmmc@users.noreply.github.com>
2022-01-03 15:35:56 +0100

    swagger

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ const bodyParser = require("body-parser"),
 const options = {
   swaggerDefinition,
   // Paths to files containing OpenAPI definitions
-  apis: ['./routes/*.js'],
+  apis: ['swagger.yml'],
 };
 const swaggerSpec = swaggerJSDoc(options);
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
