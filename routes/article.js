15d2621de3346f92da9c5b456ca4f022664421b4
mohamedmmc <46996751+mohamedmmc@users.noreply.github.com>
2022-01-04 03:59:02 +0100

    heroku

--- routes/article.js
+++ routes/article.js
@@ -60,7 +60,7 @@ router.patch ('/:id',getArticle,multer,async (req,res) => {
     }
     try {
         const updatedArticle = await res.article.save()
-        const newnewArticle = await Article.findById(updatedArticle.id).populate('user')
+        const newnewArticle = await Article.findById(updatedArticle.id).populate('user').populate('question')
         res.json(newnewArticle)
     } catch (error) {
         res.status(400).json({message : error.message})
