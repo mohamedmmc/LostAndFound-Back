6aefbcf33f4dba1b9a561d6733bbc98f4f613279
mohamedmmc <46996751+mohamedmmc@users.noreply.github.com>
2022-02-02 21:05:02 +0100

    update

--- routes/user.js
+++ routes/user.js
@@ -200,11 +200,11 @@ router.post ('/',multer,async (req,res) => {
             <tr>
                 <td bgcolor="#f4f4f4" align="center" style="padding: 0px 10px 0px 10px;">
                     <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
-                        <tr>
-                            <td bgcolor="#ffffff" align="left" style="padding: 20px 30px 40px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
-                                <p style="margin: 0;">We're excited to have you get started. First, you need to confirm your account. Just press the button below.</p>
-                            </td>
-                        </tr>
+                    <tr>
+                    <td bgcolor="#ffffff" align="left" style="padding: 20px 30px 40px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
+                        <p style="margin: 0;">Merci d'avoir creer un compte dans LostAndFound. vous pouvez confirmer votre compte en appuyant sur le bouton suivant.</p>
+                        </td>
+                </tr>
                         <tr>
                             <td bgcolor="#ffffff" align="left">
                                 <table width="100%" border="0" cellspacing="0" cellpadding="0">
@@ -719,7 +719,7 @@ router.post('/resendConfirmation/:id',getUserById,async (req,res,next) =>{
                     <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
                         <tr>
                             <td bgcolor="#ffffff" align="center" valign="top" style="padding: 40px 20px 20px 20px; border-radius: 4px 4px 0px 0px; color: #111111; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 48px; font-weight: 400; letter-spacing: 4px; line-height: 48px;">
-                                <h1 style="font-size: 48px; font-weight: 400; margin: 2;">Welcome ${hname}!</h1> <img src=" https://img.icons8.com/clouds/100/000000/handshake.png" width="125" height="120" style="display: block; border: 0px;" />
+                                <h1 style="font-size: 48px; font-weight: 400; margin: 2;">Rebonjour ${hname}!</h1> <img src=" https://img.icons8.com/clouds/100/000000/handshake.png" width="125" height="120" style="display: block; border: 0px;" />
                             </td>
                         </tr>
                     </table>
@@ -728,11 +728,11 @@ router.post('/resendConfirmation/:id',getUserById,async (req,res,next) =>{
             <tr>
                 <td bgcolor="#f4f4f4" align="center" style="padding: 0px 10px 0px 10px;">
                     <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
-                        <tr>
-                            <td bgcolor="#ffffff" align="left" style="padding: 20px 30px 40px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
-                                <p style="margin: 0;">We're excited to have you get started. First, you need to confirm your account. Just press the button below.</p>
-                            </td>
-                        </tr>
+                    <tr>
+                    <td bgcolor="#ffffff" align="left" style="padding: 20px 30px 40px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
+                        <p style="margin: 0;">Merci d'avoir creer un compte dans LostAndFound. vous pouvez confirmer votre compte en appuyant sur le bouton suivant.</p>
+                        </td>
+                </tr>
                         <tr>
                             <td bgcolor="#ffffff" align="left">
                                 <table width="100%" border="0" cellspacing="0" cellpadding="0">
@@ -1525,11 +1525,11 @@ function template(hname){
             <tr>
                 <td bgcolor="#f4f4f4" align="center" style="padding: 0px 10px 0px 10px;">
                     <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
-                        <tr>
-                            <td bgcolor="#ffffff" align="left" style="padding: 20px 30px 40px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
-                                <p style="margin: 0;">We're excited to have you get started. First, you need to confirm your account. Just press the button below.</p>
-                            </td>
-                        </tr>
+                    <tr>
+                    <td bgcolor="#ffffff" align="left" style="padding: 20px 30px 40px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
+                        <p style="margin: 0;">Merci d'avoir creer un compte dans LostAndFound. vous pouvez confirmer votre compte en appuyant sur le bouton suivant.</p>
+                        </td>
+                </tr>
                         <tr>
                             <td bgcolor="#ffffff" align="left">
                                 <table width="100%" border="0" cellspacing="0" cellpadding="0">
